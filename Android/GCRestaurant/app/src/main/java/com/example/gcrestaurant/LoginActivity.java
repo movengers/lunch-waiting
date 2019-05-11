0db4891d3c12b4749b5beb9ecef2d21099e05054
Euna <lecjgn@naver.com>
2019-05-11 19:08:15 +0900

    Change Start Activity
    Change Start Activit 
    Main activity -> Login activity
    
    and delete floating message

---
+++ Android/GCRestaurant/app/src/main/java/com/example/gcrestaurant/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.gcrestaurant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+}
