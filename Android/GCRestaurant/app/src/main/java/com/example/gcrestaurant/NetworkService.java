2fba25e95edf2669ef93f54b8c5fb2f5602a8ae7
Seunghwa <banksemi@naver.com>
2019-05-16 21:17:42 +0900

    Create Server Project

---
+++ Android/GCRestaurant/app/src/main/java/com/example/gcrestaurant/NetworkService.java
@@ -0,0 +1,88 @@
+package com.example.gcrestaurant;
+
+import android.os.Debug;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ReceiverCallNotAllowedException;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class NetworkService extends Service{
+    //public static List<ESocketActivity> receivers = new ArrayList();
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Service 객체와 (화면단 Activity 사이에서)
+        // 통신(데이터를 주고받을) 때 사용하는 메서드
+        // 데이터를 전달할 필요가 없으면 return null;
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 서비스에서 가장 먼저 호출됨(최초에 한번만)
+        Log.d("test", "서비스의 onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        /*
+        if (esocket == null)
+        {
+            esocket = new ESocket(this);
+            esocket.start();
+        }
+        */
+        Log.d("테스트1", "서비스의 onStartCommand 끝");
+        return super.onStartCommand(intent, flags, startId);
+    }
+/*
+    Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            msg.getData().get
+            List<JSONObject> list = NetworkMain.ReceiveQueue.Get();
+            if (list != null) {
+                for (JSONObject item : list) {
+
+                    ReceiveMessage(item);
+                }
+            }
+            // 메세지를 처리하고 또다시 핸들러에 메세지 전달 (1000ms 지연)
+            //mHandler.sendEmptyMessageDelayed(0,1000);
+        }
+    };
+    */
+}
--- Server/GCRestaurantServer/GCRestaurantServer/Program.cs
+++ Server/GCRestaurantServer/GCRestaurantServer/Program.cs
@@ -3,13 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NetworkLibrary;
+using Newtonsoft.Json.Linq;
 namespace GCRestaurantServer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Server server = new Server(1231);
+            server.Connect += Server_Connect;
+            server.Receive += Server_Receive_Try;
+            server.Exit += Server_Exit;
+
+           
+            while (true)
+            {
+                System.Threading.Thread.Sleep(4000);
+            }
+        }
+
+        private static void Server_Connect(ESocket socket)
+        {
+            Console.WriteLine("로그인");
+        }
+        private static void Server_Receive_Try(ESocket socket, JObject Message)
+        {
+            try
+            {
+                Server_Receive(socket, Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\r\n" + e.StackTrace);
+            }
+        }
+        private static void Server_Receive(ESocket socket, JObject Message)
+        {
+            switch ((int)Message["type"])
+            {
+                case 1000:
+                    Console.WriteLine("안드로이드 테스트 메세지 : " + (string)Message["message"]);
+                    break;
+            }
+        }
+
+
+
+        private static void Server_Exit(ESocket socket)
+        {
+            Console.WriteLine("누군가 종료함");
         }
     }
 }
