0e481a0d46075bdf737aba98348736e186f0efc0
Euna <lecjgn@naver.com>
2019-05-17 18:38:51 +0900

    Create bottom menu
    add 5 bottom naviation menu
    1. Home
    2. lunch waiting
    3. ranking
    4. board
    5. setting

--- Android/GCRestaurant/app/src/main/java/com/example/gcrestaurant/MainActivity.java
+++ Android/GCRestaurant/app/src/main/java/com/example/gcrestaurant/MainActivity.java
@@ -130,18 +130,8 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.menu_logout) {
+            // 로그아웃
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
