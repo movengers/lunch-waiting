7d1cdf5282f1ca45670bf4934fe24d472507e908
Seunghwa <banksemi@naver.com>
2019-05-24 18:07:12 +0900

    Implement Naver API Module
    I use the Naver API to get the names of restaurants around Gachon univ.
    
    We'll get more information through crawling.

---
+++ Server/GCRestaurantServer/GCRestaurantServer/Module/NaverAPIModule.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using System.Web;
+using HtmlAgilityPack;
+namespace GCRestaurantServer
+{
+    public static class NaverAPIModule
+    {
+        public static JObject SearchPlace(string client_id, string client_key, string keyword)
+        {
+            HttpWebRequest hreq = (HttpWebRequest)WebRequest.Create("https://openapi.naver.com/v1/search/local?query="+ HttpUtility.HtmlEncode(keyword) + "&display=30&start=1&sort=random");
+            hreq.Method = "GET";
+            hreq.ContentType = "plain/text;charset=utf-8";
+            hreq.Headers["X-Naver-Client-Id"] = client_id;
+            hreq.Headers["X-Naver-Client-Secret"] = client_key;
+            HttpWebResponse hres = (HttpWebResponse)hreq.GetResponse();
+            if (hres.StatusCode == HttpStatusCode.OK)
+            {
+                Stream dataStream = hres.GetResponseStream();
+                StreamReader sr = new StreamReader(dataStream, Encoding.UTF8);
+                string result = sr.ReadToEnd();
+                dataStream.Close();
+                sr.Close();
+                return JObject.Parse(result);
+            }
+            return null;
+        }
+
+        public static string GetPlaceDescription(int id)
+        {
+            JObject result = GetInfomationDetail(id);
+            return (string)result["business"][id.ToString()]["base"]["description"];
+        }
+
+        public static JObject GetInfomationDetail(int id)
+        {
+            HtmlDocument dom = ParseSupport.Crawling("https://store.naver.com/restaurants/detail?id=" + HttpUtility.HtmlEncode(id));
+            HtmlNode ds = dom.DocumentNode.SelectSingleNode("//body/script");
+
+            string result = ParseSupport.UTF8Char((string)ds.InnerHtml);
+            result = ds.InnerHtml.Substring(result.IndexOf("=") + 1);
+            return JObject.Parse(result);
+        }
+
+        public static JArray GetPlaceMenu(int id)
+        {
+            JObject result = GetInfomationDetail(id);
+            JArray menus =(JArray)result["business"][id.ToString()]["biz"]["menus"];
+            return menus;
+        }
+        public static int GetPlaceID(string RestaurantTitle)
+        {
+            HtmlDocument dom = ParseSupport.Crawling("https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + HttpUtility.HtmlEncode(RestaurantTitle));
+            HtmlNode ds = dom.DocumentNode.SelectSingleNode("//a[@class='api_more_theme']");
+
+            return (int)ParseSupport.UrlQueryParser(ds)["id"];
+        }
+        public static int GetPlaceID(string RestaurantTitle, string roadAddress)
+        {
+            HtmlDocument dom = ParseSupport.Crawling("https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + HttpUtility.HtmlEncode(roadAddress));
+            HtmlNodeCollection ds = dom.DocumentNode.SelectNodes("//dd[@class='vicinity']/a");
+            foreach (HtmlNode node in ds)
+            {
+                if (node.InnerText == RestaurantTitle)
+                {
+                    return (int)ParseSupport.UrlQueryParser(node)["code"];
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Server/GCRestaurantServer/GCRestaurantServer/Thread/AutoCrawling.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using EasyMysql;
+namespace GCRestaurantServer
+{
+    class AutoCrawling
+    {
+        private static String[] keywords = new string[] { "가천대 맛집", "가천대 식당"};
+        public static void main()
+        {
+            Program.LogSystem.AddLog(2, "AutoCrawling", "네이버를 통해 음식점 데이터를 갱신합니다");
+
+            foreach (string keyword in keywords)
+            {
+                Program.LogSystem.AddLog(1, "AutoCrawling", keyword + " 키워드로 검색 시작");
+                JObject search_data = NaverAPIModule.SearchPlace((string)ConfigManagement.GetObject("naver_api")["client_id"], 
+                    (string)ConfigManagement.GetObject("naver_api")["client_secret"],
+                    keyword);
+                foreach (JObject restaurant in search_data["items"])
+                {
+
+                    MysqlNode update = new MysqlNode(Program.mysqlOption, "INSERT INTO restaurant (no, title, roadAddress, mapx, mapy, category) VALUES (?no, ?title, ?roadAddress, ?mapx, ?mapy, ?category)");
+                    update["title"] = Regex.Replace((string)restaurant["title"], "(<[/a-zA-Z]+>)", "");
+                    update["roadAddress"] = (string)restaurant["roadAddress"];
+                    update["mapx"] = (string)restaurant["mapx"];
+                    update["mapy"] = (string)restaurant["mapy"];
+                    update["category"] = (string)restaurant["category"];
+
+                    update["no"] = NaverAPIModule.GetPlaceID((string)update["title"], (string)update["roadAddress"]);
+                    update.ExecuteNonQuery();
+                    Program.LogSystem.AddLog(1, "AutoCrawling", update["title"] + " 를 리스트에 등록");
+
+                    // 메뉴 갱신
+                }
+            }
+            Program.LogSystem.AddLog(1, "AutoCrawling", "ra");
+           
+            int id = NaverAPIModule.GetPlaceID("원조 태평동 곱창");
+            NaverAPIModule.GetPlaceMenu(id);
+
+            string description = NaverAPIModule.GetPlaceDescription(id);
+            JObject jsoan = NaverAPIModule.GetInfomationDetail(id);
+        }
+    }
+}
