ba949575dc442ab21a6e6137e6d4ce92e41e9c6d
Euna <98dmsdk@naver.com>
2019-05-26 01:01:46 +0900

    Add Tab Menu to Ranking Menu
    Add Tab Menu to Ranking Menu

--- Android/GCRestaurant/app/src/main/java/com/example/gcrestaurant/Menu_Ranking.java
+++ Android/GCRestaurant/app/src/main/java/com/example/gcrestaurant/Menu_Ranking.java
@@ -1,16 +1,43 @@
 package com.example.gcrestaurant;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
 
 public class Menu_Ranking extends Fragment {
+    private Context mContext;
+    private TabLayout mTabLayout;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu_ranking,container, false);
+        View view = inflater.inflate(R.layout.fragment_menu_ranking,container, false);
+        mContext = getContext();
+
+        mTabLayout = (TabLayout) view.findViewById(R.id.layout_tab);
+
+        mTabLayout.addTab(mTabLayout.newTab().setText("한식"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("분식"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("돈까스,회,일식"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("치킨"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("피자"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("중국집"));
+
+        return view;
+    }
+
+    private View createTabView(String tabName) {
+        View tabView = LayoutInflater.from(mContext).inflate(R.layout.custom_tab, null);
+        TextView txt_name = (TextView) tabView.findViewById(R.id.txt_name);
+        txt_name.setText(tabName);
+        return tabView;
+
     }
 }
