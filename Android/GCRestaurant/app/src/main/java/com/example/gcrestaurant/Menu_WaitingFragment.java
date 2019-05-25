d4c856fb8e83b47b368651778097325f2aa6a73a
Seunghwa <banksemi@naver.com>
2019-05-26 07:48:49 +0900

    Add Google Map (API)

--- Android/GCRestaurant/app/src/main/java/com/example/gcrestaurant/Menu_WaitingFragment.java
+++ Android/GCRestaurant/app/src/main/java/com/example/gcrestaurant/Menu_WaitingFragment.java
@@ -3,17 +3,63 @@ package com.example.gcrestaurant;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Menu_WaitingFragment extends Fragment {
+import com.google.android.gms.maps.MapFragment;
+
+public class Menu_WaitingFragment extends Fragment implements OnMapReadyCallback {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         Toast.makeText(getContext(), "눌림", Toast.LENGTH_LONG).show();
-        return inflater.inflate(R.layout.fragment_menu_waiting,container, false);
+        View view = inflater.inflate(R.layout.fragment_menu_waiting,container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_f);
+        NetworkService.SendDebugMessage(getChildFragmentManager().getFragments().toString());
+        mapFragment.getMapAsync(this);
+     //   mapFragment.getMapAsync(this);
+
+    }
+    @Override
+    public void onMapReady(final GoogleMap map) {
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        map.addMarker(markerOptions);
+
+        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        map.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 }
