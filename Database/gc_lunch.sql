428ed6808f976a6a1fbff56a72df16c9f01e42fa
Seunghwa <banksemi@naver.com>
2019-05-25 09:23:26 +0900

    Add DB SQL File
    There is a table to store crawling data and user ID link.

---
+++ Database/gc_lunch.sql
@@ -0,0 +1,38 @@
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for menu
+-- ----------------------------
+CREATE TABLE `menu` (
+  `restaurant_no` int(11) NOT NULL,
+  `priority` int(11) NOT NULL,
+  `name` char(50) NOT NULL DEFAULT '',
+  `price` int(11) NOT NULL,
+  `description` text DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`restaurant_no`,`priority`),
+  CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`restaurant_no`) REFERENCES `restaurant` (`no`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for restaurant
+-- ----------------------------
+CREATE TABLE `restaurant` (
+  `no` int(11) NOT NULL,
+  `title` char(50) NOT NULL,
+  `category` char(40) DEFAULT NULL,
+  `roadAddress` char(60) DEFAULT '',
+  `image` varchar(100) DEFAULT NULL,
+  `mapx` int(11) DEFAULT NULL,
+  `mapy` int(11) DEFAULT NULL,
+  PRIMARY KEY (`no`),
+  UNIQUE KEY `name` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` char(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
