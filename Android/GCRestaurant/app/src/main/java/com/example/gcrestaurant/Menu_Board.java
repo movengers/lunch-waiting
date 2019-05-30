5ba7bde3cb294a95596d201c7ed13ea9cbd4c20e
Jeong You Ji <dbwl150236@naver.com>
2019-05-30 23:53:25 +0900

    Modify Add Question Item
    modify add question Item

--- Android/GCRestaurant/app/src/main/java/com/example/gcrestaurant/Menu_Board.java
+++ Android/GCRestaurant/app/src/main/java/com/example/gcrestaurant/Menu_Board.java
@@ -8,11 +8,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
 public class Menu_Board extends Fragment {
     RecyclerView expanderRecyclerView;
+    ImageButton qBtnAdd;
+    EditText questionAdd;
 
     @Nullable
     @Override
@@ -20,18 +25,19 @@ public class Menu_Board extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_menu_board, container, false);
         expanderRecyclerView = view.findViewById(R.id.card_view);
-        initiateExpander();
+        initiateExpander(view);
         return view;
     }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
-    private void initiateExpander() {
-        ArrayList<String> parentList = new ArrayList<>();
-        ArrayList<ArrayList> childListHolder = new ArrayList<>();
+    private void initiateExpander(View view) {
+        final ArrayList<String> parentList = new ArrayList<>();
+        final ArrayList<ArrayList> childListHolder = new ArrayList<>();
+        qBtnAdd =  view.findViewById(R.id.qBtn);
+        questionAdd = view.findViewById(R.id.edit_question);
 
         parentList.add("Question");
         parentList.add("Question2");
@@ -43,7 +49,7 @@ public class Menu_Board extends Fragment {
         parentList.add("Question8");
 
         ArrayList<String> childList = new ArrayList<>();
-        childList.add("answer");
+        childList.add("answer1");
         childList.add("answer1-2");
         childList.add("answer1-3");
 
@@ -98,12 +104,27 @@ public class Menu_Board extends Fragment {
 
         childListHolder.add(childList);
 
-        ExpandableRecyclerViewAdpater expandableCategoryRecyclerViewAdapter =
+        final ExpandableRecyclerViewAdpater expandableCategoryRecyclerViewAdapter =
                 new ExpandableRecyclerViewAdpater(getActivity().getApplicationContext(), parentList,
                         childListHolder);
 
         expanderRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
 
         expanderRecyclerView.setAdapter(expandableCategoryRecyclerViewAdapter);
+
+        qBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = 0;
+                ArrayList<String> childList = new ArrayList<>(); // 댓글 arrayList
+
+                String question = questionAdd.getText().toString(); // 질문 edittext 값 받아오기
+                parentList.add(position, "" + question); // 질문 parentList에 추가
+                childListHolder.add(position, childList); // 빈 댓글 arrayList 추가
+
+                expandableCategoryRecyclerViewAdapter.notifyItemInserted(position);
+                expanderRecyclerView.scrollToPosition(position);
+            }
+        });
     }
 }
