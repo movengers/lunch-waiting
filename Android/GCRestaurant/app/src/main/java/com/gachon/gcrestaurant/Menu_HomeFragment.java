8b61d265082099abc3186a201d2b438c4fa363f6
Euna <98dmsdk@naver.com>
2019-06-02 14:49:19 +0900

    Edit Home screen Restaurant List
    Edit home screen restaurant list

--- Android/GCRestaurant/app/src/main/java/com/gachon/gcrestaurant/Menu_HomeFragment.java
+++ Android/GCRestaurant/app/src/main/java/com/gachon/gcrestaurant/Menu_HomeFragment.java
@@ -8,18 +8,49 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.RemoteViews;
 
 public class Menu_HomeFragment extends Fragment {
-    public RemoteViews contentView;
-    public Intent intent1,intent2;
-    public PendingIntent pintent1,pintent2 ;
+    private ListViewHomeRestAdapter adapter;
+    private ListView listView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_menu_home, null) ;
+
+        listView = view.findViewById(R.id.menu_rest_list);
+        adapter = new ListViewHomeRestAdapter();
+
+        listView.setAdapter(adapter);
 
 
-        return inflater.inflate(R.layout.fragment_menu_home,container, false);
+
+
+        return view;
+    }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
+
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
